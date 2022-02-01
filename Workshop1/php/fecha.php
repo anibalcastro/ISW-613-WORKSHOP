84ec6b70461f228651a0da41d7d218ba6767418e
anibalcastro <anibalcastro1515@gmail.com>
2022-02-01 11:20:05 -0600

    update functions

--- Workshop1/php/fecha.php
+++ Workshop1/php/fecha.php
@@ -1,11 +1,25 @@
 <?php
-    //Obteniendo la fecha actual
-    $fechaActual = date('d-m-Y');
 
-    //Obteniendo hora Actual
-    $horaActual = time();
+    //Obtiene fecha
+    function obtenerFecha(){
+        //variable almacena fecha
+        $fechaActual = date('d-m-Y');
 
+        return $fechaActual;
+    }
+    
+    //Obtener hora
+    function obtenerHora(){
+        //variable almacena hora
+        $horaActual = date("h:i:s a");
+
+        return $horaActual;
+    }
+
+
+    $fecha = obtenerFecha();
+    $hora = obtenerHora();
     //imprimir fechaActual y hora actual
-    echo $fechaActual ."<br>". $horaActual;
+    echo $fecha ."<br>". $hora;
 
 ?>
