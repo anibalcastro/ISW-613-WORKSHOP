3b9d90ddcd4b8629e67af15335cd7b3a415b4656
anibalcastro <anibalcastro1515@gmail.com>
2022-02-01 10:22:28 -0600

    php

---
+++ Workshop1/php/fecha.php
@@ -0,0 +1,12 @@
+<?php
+    //Obteniendo la fecha actual
+    $fechaActual = date('d-m-Y');
+
+    //Obteniendo hora Actual
+    $horaActual = time();
+
+    //imprimir fechaActual
+    echo $fechaActual;
+    //imprimir horaActual
+    echo $horaActual;
+?>
