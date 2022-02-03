60b1f2d6901bfcea437885e2ba9c3135219319f9
anibalcastro <anibalcastro1515@gmail.com>
2022-02-02 19:34:01 -0600

    zona horaria

--- Workshop1/php/fecha.php
+++ Workshop1/php/fecha.php
@@ -3,7 +3,8 @@
     //Obtiene fecha
     function obtenerFecha(){
         //variable almacena fecha
-        $fechaActual = date('d-m-Y');
+        date_default_timezone_set("America/Costa_Rica");
+        $fechaActual = date("F j, Y");
 
         return $fechaActual;
     }
@@ -20,6 +21,6 @@
     $fecha = obtenerFecha();
     $hora = obtenerHora();
     //imprimir fechaActual y hora actual
-    echo $fecha ."<br>". $hora;
+    echo "<p>La fecha actual es: </p>", $fecha,"<br>", "<p>La Hora actual es:</p>", $hora;
 
 ?>
