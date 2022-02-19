dbfaf8e36701dddf19b86d4ead6580095a59ad89
anibalcastro <anibalcastro1515@gmail.com>
2022-02-18 23:31:44 -0600

    update link bootstrap

--- Workshop3/header.php
+++ Workshop3/header.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <a class="navbar-brand">UTN ISW613</a>
   <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
--- Workshop3/signup.php
+++ Workshop3/signup.php
@@ -7,7 +7,7 @@
     $sql = "INSERT INTO categories(`nombre`, `descripcion`) VALUES ('$nombre','$descripcion')";
     $connection = mysqli_connect('127.0.0.1','root','','workshop3');
     mysqli_query($connection, $sql);
-    header('Location: /index1.php?status=success&message=User was created');
+    header('Location: http://utnweb.com/web2/ISW-613-WORKSHOP/Workshop3/index.php?status=success&message=User was created');
   } else {
-    header('Location: /index1.php?status=error&message=There was an error');
+    header('Location: http://utnweb.com/web2/ISW-613-WORKSHOP/Workshop3/index.php?status=error&message=There was an error');
   }
