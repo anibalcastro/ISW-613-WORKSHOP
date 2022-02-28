81a847cace52fba38dbb88bc6e804ecebc34ea4a
anibalcastro <anibalcastro1515@gmail.com>
2022-02-27 21:24:34 -0600

    authenticate

--- Workshop4/function.php
+++ Workshop4/function.php
@@ -36,3 +36,30 @@ function getMatriculas(){
   $matriculas = $result->fetch_all(MYSQLI_ASSOC);
   return $matriculas;
 }
+
+/**
+ * Valida si el usuario ingresado
+ * existe en la base de datos
+ * por medio del $email y $password
+ */
+function authenticate($email, $password){
+  //falta colocar datos
+  $connection = mysqli_connect('localhost:3306', 'root', 'root1234', 'php_web2');
+
+  //consulta para saber existencia
+  $sqlUsuarios = "SELECT * FROM users WHERE  `username` = '$username' AND `password` = '$password'";
+
+  //resultado si el existe o no
+  $result = mysqli_query($connection, $sqlUsuarios);
+
+  //si el sql viene vacio, cierra conexión y retorna falso.
+  if ($result == ""){
+    mysqli_close($connection);
+    return false;
+  }
+  // si viene lleno, cierra conexión y retorna el resultado.
+  else{
+    mysqli_close($connection);
+    return $result->fetch_array();
+  }
+}
