847b6fd8805fe7dbd1df0acc8bf0384d4f8f26a6
anibalcastro <anibalcastro1515@gmail.com>
2022-03-01 12:01:12 -0600

    comment

--- Workshop5/temperatures.php
+++ Workshop5/temperatures.php
@@ -1,25 +1,47 @@
 <?php
+//******************************************************************************* */
+//******************************************************************************* */
+//Arreglo inicial de las temperaturas
 $temperatures = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
 
+//Averiguo tamaño
 $tamaño = sizeof($temperatures);
+//Inicializo la variable suma
 $suma = 0;
 
-
+/**
+ * Recorro el arreglo temperatures
+ * Suma
+ */
 foreach($temperatures as $it){
-    $suma = $suma + $it;
+    $suma += $it;
 }
 
+//Se almacena el promedio
 $promedio = $suma/$tamaño;
 
-//Promedio
+//Imprime el promedio
 echo "Average Temperature is : $promedio". PHP_EOL; 
 
+//******************************************************************************* */
+//******************************************************************************* */
 
 //menores
+//Ordeno de manera ascendente
 asort($temperatures);
 
+//Arreglo donde almacena menores
 $menores = array();
+
+//Iterador
 $iterador = 0;
+
+/**
+ * Recorre el arreglo
+ * Valida si ya lleva 5 elementos
+ * Pregunta si ya existe
+ * Agrega al arreglo
+ */
 foreach($temperatures as $min){
     if ($iterador === 5){
         $iterador = 0;
@@ -32,13 +54,25 @@ foreach($temperatures as $min){
     }
 }
 
+//Convierto array en string
 $sMenores = implode(",", $menores);
+//Imprimo
 echo "List of 5 lowest temperatures (no duplicates): $sMenores". PHP_EOL;
 
+//******************************************************************************* */
+//******************************************************************************* */
 //mayores
+//Ordeno de manera descendentee
 arsort($temperatures);
+//Arreglo que almacena mayores
 $mayores = array();
 
+/**
+ * Recorre el arreglo
+ * Valida si ya lleva 5 elementos
+ * Pregunta si ya existe
+ * Agrega al arreglo
+ */
 foreach($temperatures as $max){
     if ($iterador === 5){
         $iterador = 0;
@@ -51,6 +85,8 @@ foreach($temperatures as $max){
     }
 }
 
+//Convierto array en string
 $sMayores = implode(",", $mayores);
+//Imprimo
 echo "List of 5 highest temperatures (no duplicates): $sMayores". PHP_EOL;
 
