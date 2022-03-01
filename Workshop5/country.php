ce5f276dac012a38f9a9b65134fa5de19f8bcec4
anibalcastro <anibalcastro1515@gmail.com>
2022-03-01 12:02:48 -0600

    comment code

--- Workshop5/country.php
+++ Workshop5/country.php
@@ -1,14 +1,13 @@
 <?php
-//1) Create a PHP script which display the capital and country name from the above array $ceu. Sort the list by the name of the country.
-
+//Arreglo con llave y valor
 $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
 
+//Ordeno las llaves de forma ascendente
 ksort($ceu);
 
+//Imprimo de forma ascendente
 foreach($ceu as $country =>$capital){
-    echo "The capital of $country is  $capital".PHP_EOL;
+    echo "The capital of $country is  $capital". PHP_EOL;
 }
 
-//Sample Output :
-//The capital of Netherlands is Amsterdam 
 
